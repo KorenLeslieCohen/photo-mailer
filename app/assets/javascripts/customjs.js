58dc70e6b85b0a51608e544035d642de9ca76e2c
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-07-31 16:03:45 -0400

    template working and js code in but commented due to issue

---
+++ app/assets/javascripts/customjs.js
@@ -0,0 +1,38 @@
+// $('.photo').append("<div id='new-photo'></div>");
+// $('#new-photo').css({"background": "red", "width": "300px"});
+// click will actually click on the below div
+
+// $(document).ready(function(){
+// $('#card_photo').append("<div id='new-photo'></div>");
+// $('.photo').css({"border": "1px solid #239AFB", "color": "white", "height": "100px", "width": "200px"});
+// $('.photo').click(function(){
+//   $('#card_photo').click();
+// });
+
+
+  
+
+
+
+// });
+
+
+
+// .submit-card input {
+//   border: 1px solid #239AFB;
+//   color: white;
+//   background: red;
+//   padding: 10px;
+//   font-size: 20px;
+//   font-family: 'Shadows Into Light Two';
+//   transition-duration: .3s;
+//   width: 300px;
+// }
+
+// .submit-card input:hover {
+//   background: white;
+//   color: red;
+//   transition-property: background, border, color;
+//   transition-duration: .3s;
+// }
+

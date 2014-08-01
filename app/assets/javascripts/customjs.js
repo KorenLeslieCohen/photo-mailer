f40365e14ecd773fae7d61cc7abef74cf0c6f8f5
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-01 15:29:56 -0400

    javascript home delay time

--- app/assets/javascripts/customjs.js
+++ app/assets/javascripts/customjs.js
@@ -1,11 +1,11 @@
 // index page intro
 $(document).ready(function(){
-  $('#one').hide().delay(500).fadeIn("slow").fadeOut("slow");
-  $('#two').hide().delay(2000).fadeIn("slow").fadeOut("slow");
-  $('#three').hide().delay(3500).fadeIn("slow").fadeOut("slow");
-  $('#four').hide().delay(5000).fadeIn("slow").fadeOut("slow");
-  $('#five').hide().delay(6500).fadeIn("slow").fadeOut("slow");
-  $('#six').hide().delay(8000).fadeIn("slow").fadeOut("slow");
+  $('#one').hide().delay(1000).fadeIn("slow").fadeOut("slow");
+  $('#two').hide().delay(2500).fadeIn("slow").fadeOut("slow");
+  $('#three').hide().delay(4000).fadeIn("slow").fadeOut("slow");
+  $('#four').hide().delay(5500).fadeIn("slow").fadeOut("slow");
+  $('#five').hide().delay(7000).fadeIn("slow").fadeOut("slow");
+  $('#six').hide().delay(8500).fadeIn("slow").fadeOut("slow");
 });
 
 

d8a6a4c16bf3ebf4069406ca3cbce1d56155c8a6
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-01 02:59:24 -0400

    javascript on index

--- app/assets/javascripts/customjs.js
+++ app/assets/javascripts/customjs.js
@@ -2,7 +2,7 @@
 // $('#new-photo').css({"background": "red", "width": "300px"});
 // click will actually click on the below div
 
-// $(document).ready(function(){
+$(document).ready(function(){
 // $('#card_photo').append("<div id='new-photo'></div>");
 // $('.photo').css({"border": "1px solid #239AFB", "color": "white", "height": "100px", "width": "200px"});
 // $('.photo').click(function(){
@@ -11,11 +11,15 @@
 
 
   
-// $('#one').show().delay(2000).hide();
-// $('#two').hide().delay(2000).show();
+$('#one').hide().delay(500).fadeIn("slow").fadeOut("slow");
+$('#two').hide().delay(2000).fadeIn("slow").fadeOut("slow");
+$('#three').hide().delay(3500).fadeIn("slow").fadeOut("slow");
+$('#four').hide().delay(5000).fadeIn("slow").fadeOut("slow");
+$('#five').hide().delay(6500).fadeIn("slow").fadeOut("slow");
+$('#six').hide().delay(8000).fadeIn("slow").fadeOut("slow");
+// $('#seven').hide().delay(9500).fadeIn("slow").fadeOut("slow");
 
-
-// });
+});
 
 
 

a29483c43a4462a2bf4ec64246684011c16daa18
github username <github email address>
2014-08-01 10:06:14 -0400

    added email format validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,6 +8,9 @@ validates :photo, :attachment_presence => true
 validates_with AttachmentSizeValidator, :attributes => :photo, :less_than => 3.megabytes
 validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 validates_presence_of :sender_email, :sender_name, :recipient_name, :recipient_email, :message
+validates :sender_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+validates :recipient_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+                                             
 
 def s3_credentials
   { :access_key_id => AWS_ACCESS_KEY_ID, :secret_access_key => AWS_SECRET_ACCESS_KEY}

f0d298ae415c0ad74800020ecb26a0e3b7960864
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-01 14:26:40 -0400

    cleaned css file and email template

--- app/assets/javascripts/customjs.js
+++ app/assets/javascripts/customjs.js
@@ -1,43 +1,13 @@
-// $('.photo').append("<div id='new-photo'></div>");
-// $('#new-photo').css({"background": "red", "width": "300px"});
-// click will actually click on the below div
-
+// index page intro
 $(document).ready(function(){
-// $('#card_photo').append("<div id='new-photo'></div>");
-// $('.photo').css({"border": "1px solid #239AFB", "color": "white", "height": "100px", "width": "200px"});
-// $('.photo').click(function(){
-//   $('#card_photo').click();
-// });
-
-
-  
-$('#one').hide().delay(500).fadeIn("slow").fadeOut("slow");
-$('#two').hide().delay(2000).fadeIn("slow").fadeOut("slow");
-$('#three').hide().delay(3500).fadeIn("slow").fadeOut("slow");
-$('#four').hide().delay(5000).fadeIn("slow").fadeOut("slow");
-$('#five').hide().delay(6500).fadeIn("slow").fadeOut("slow");
-$('#six').hide().delay(8000).fadeIn("slow").fadeOut("slow");
-// $('#seven').hide().delay(9500).fadeIn("slow").fadeOut("slow");
-
+  $('#one').hide().delay(500).fadeIn("slow").fadeOut("slow");
+  $('#two').hide().delay(2000).fadeIn("slow").fadeOut("slow");
+  $('#three').hide().delay(3500).fadeIn("slow").fadeOut("slow");
+  $('#four').hide().delay(5000).fadeIn("slow").fadeOut("slow");
+  $('#five').hide().delay(6500).fadeIn("slow").fadeOut("slow");
+  $('#six').hide().delay(8000).fadeIn("slow").fadeOut("slow");
 });
 
 
 
-// .submit-card input {
-//   border: 1px solid #239AFB;
-//   color: white;
-//   background: red;
-//   padding: 10px;
-//   font-size: 20px;
-//   font-family: 'Shadows Into Light Two';
-//   transition-duration: .3s;
-//   width: 300px;
-// }
-
-// .submit-card input:hover {
-//   background: white;
-//   color: red;
-//   transition-property: background, border, color;
-//   transition-duration: .3s;
-// }
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "koren.cohen@gmail.com"
 
-  # def welcome_email(user)
-  #   @user = user
-  #   @url  = 'http://example.com/login'
-  #   mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  # end
-
   def welcome_email(card)
     @card = card
     @url  = 'http://example.com/login'
